587420ffdd76b1b0a46304888d9cd53dc71afe18
totdking <timileyindgreat02@gmail.com>
2025-12-14 13:43:23 +0100

    ceaser cipher algorithm

---
+++ src/ceaser_cipher/mod.rs
@@ -0,0 +1,1 @@
+pub mod ceaser_circuit;
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@
 
 // mod flt_eea;
 // use flt_eea::fermat_little_theorem;
+mod ceaser_cipher;
+use ceaser_cipher::ceaser_circuit::ceaser_cipher;
 fn main() {
+    let _ = ceaser_cipher("ITQZ FUXF BMPPXQ MFFUFGPQ");
+    // //
+
 }
